dc723a934bcd1cf32fcef52ce4ae0f9eea11c602
Marcos Antonio <peas.shadow@gmail.com>
2022-11-06 12:06:46 -0300

    implementando os testes de order e orderItem

--- src/entity/order.spec.ts
+++ src/entity/order.spec.ts
@@ -1,6 +1,7 @@
 import Customer from "./customer";
 import Order from "./order";
 import OrderItem from "./order_item";
+import Product from "./product";
 
 describe("Order unit tests", () => {
     it("Should throw error when id is empty", () => {
@@ -19,15 +20,22 @@ describe("Order unit tests", () => {
         }).toThrowError("Items are required");
     });
     it("Should calculate total", () => {
-        const item1 = new OrderItem("1", "item1", 10);
-        const item2 = new OrderItem("2", "item2", 12);
+        const item1 = new OrderItem("1", "item1", 10, "p1", 2);
+        const item2 = new OrderItem("2", "item2", 12, 'p1', 2);
         const order1 = new Order("1", "1", [item1]);
 
         const total1 = order1.total();
-        expect(total1).toBe(10);
+        expect(total1).toBe(20);
 
         const order2 = new Order("2", "1", [item1, item2]);
         const total2 = order2.total();
-        expect(total2).toBe(22);
+        expect(total2).toBe(44);
+    });
+    it("Should throw error if item quantity is less or equals zero", () => {
+        expect(() => {
+            const item1 = new OrderItem("1", "item1", 10, "p1", 10);
+            const item2 = new OrderItem("2", "item2", 10, "p1", 0);
+            new Order("1", "1", [item1, item2]);
+        }).toThrowError("Quantity must be greater than zero");
     });
 });
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
-import Address from "./entity/address";
+/*import Address from "./entity/address";
 import Customer from "./entity/customer";
 import Order from "./entity/order";
 import OrderItem from "./entity/order_item";
 
 let customer = new Customer("123", "Marcos Santos");
 const address = new Address("Rua Bela Vista", 690, "65605-220", "Caxias");
-customer._address = address;
+customer.address = address;
 customer.activate();
 
 const item1 = new OrderItem("1", "Ata", 10);
 const item2 = new OrderItem("2", "Uva", 12);
 
-const order = new Order("1", customer._id, [item1, item2]);
+const order = new Order("1", customer.id, [item1, item2]);*/
