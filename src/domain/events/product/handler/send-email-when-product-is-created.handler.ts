e2e90da1e4d4b9eea991797f3d0e2f455534ad0c
Marcos Antonio <peas.shadow@gmail.com>
2022-11-10 12:34:00 -0300

    removendo console.log do código

--- src/domain/events/product/handler/send-email-when-product-is-created.handler.ts
+++ src/domain/events/product/handler/send-email-when-product-is-created.handler.ts
@@ -4,7 +4,6 @@ import ProductCreatedEvent from "../product-created.event";
 export default class SendEmailWhenProductIsCreatedHandler implements EventHandlerInterface<ProductCreatedEvent> {
 
     handle(event: ProductCreatedEvent): void {
-        console.log("E-mail enviado...");
     }
 
 }
