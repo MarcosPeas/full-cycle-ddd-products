6c44872b0bbe226a49b8ffa2056ab4d9b50ae979
Marcos Antonio <peas.shadow@gmail.com>
2022-11-10 12:41:48 -0300

    ajustando descrição do tests de orderRepository

--- src/infrastructure/repository/order.repository.spec.ts
+++ src/infrastructure/repository/order.repository.spec.ts
@@ -202,7 +202,7 @@ describe("Order repository test", () => {
     });
   });
 
-  it("should find all customers", async () => {
+  it("should find all orders", async () => {
     const customerRepository = new CustomerRepository();
     const customer = new Customer("123", "Customer 1");
     const address = new Address("Street 1", 1, "Zipcode 1", "City 1");
