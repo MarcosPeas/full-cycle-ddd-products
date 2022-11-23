2df2a0d6f8d93113b52c6fcd43290f46a48e0947
Marcos Antonio <peas.shadow@gmail.com>
2022-11-23 11:47:50 -0300

    adicionando NotificationPattern em Product

--- src/domain/product/entity/product.spec.ts
+++ src/domain/product/entity/product.spec.ts
@@ -1,19 +1,25 @@
 import Product from "./product";
 
 describe("Product unit tests", () => {
-    it("Should throw error when id is empty", () => {
+    it("should throw error when id is empty", () => {
         expect(() => {
             new Product("", "Product 1", 20);
-        }).toThrowError("Id is required");
+        }).toThrowError("product: Id is required");
     });
-    it("Should throw error when name is empty", () => {
+    it("should throw error when name is empty", () => {
         expect(() => {
             new Product("1", "", 20);
-        }).toThrowError("Name is required");
+        }).toThrowError("product: Name is required");
     });
-    it("Should throw error when price is less then zero", () => {
+    it("should throw error when price is less then zero", () => {
         expect(() => {
             new Product("1", "1", -1);
-        }).toThrowError("Price must be greater than zero");
+        }).toThrowError("product: Price must be greater than zero");
+    });
+
+    it("should throw error when name and id are empty", () => {
+        expect(() => {
+            let customer = new Product("", "", 20);
+        }).toThrowError("product: Id is required, product: Name is required");
     });
 });
